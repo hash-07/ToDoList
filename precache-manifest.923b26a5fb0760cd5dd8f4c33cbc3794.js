80fd4aa40eea66b535e4f870ec75fc7799dd51f4
hasheer <hasheer11@gmail.com>
2020-07-30 17:51:06 +0530

    Updates

---
+++ precache-manifest.923b26a5fb0760cd5dd8f4c33cbc3794.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ce7aa9f01365faa61d2a310669b90c",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "27661805954fb69c5d30",
+    "url": "/ToDoList/static/css/main.1afa0a19.chunk.css"
+  },
+  {
+    "revision": "4e763c1524d1b08578a6",
+    "url": "/ToDoList/static/js/2.2508f5c4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ToDoList/static/js/2.2508f5c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27661805954fb69c5d30",
+    "url": "/ToDoList/static/js/main.92c403cd.chunk.js"
+  },
+  {
+    "revision": "416772cde36426580eb8",
+    "url": "/ToDoList/static/js/runtime-main.db9d0848.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ToDoList/static/media/logo.5d5d9eef.svg"
+  }
+]);
